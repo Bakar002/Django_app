817d8f629e98f29e141a34732ea2ae38e50479fe
Bakar002 <abubakaramjad78600@gmail.com>
2024-10-07 17:30:43 +0500

    page and app making

--- DjangoApp/posts/views.py
+++ DjangoApp/posts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Post
 
+
 # Create your views here.
 def posts_list(request):
     # Retrieve all posts and order them by 'date' field in descending order.
     posts = Post.objects.all().order_by('-date')  # '-' indicates descending order
     return render(request, 'posts/posts_list.html', {'posts': posts})
+
+# Correct indentation for post_page function
+def post_page(request, slug):
+    # Retrieve all posts and order them by 'date' field in descending order.
+    post = Post.objects.get(slug=slug) 
+    return render(request, 'posts/posts_page.html', {'post': post})
+
