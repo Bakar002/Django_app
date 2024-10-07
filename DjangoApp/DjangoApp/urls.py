a9732bd61cf012098afd69169141cb5a02e46c82
Bakar002 <abubakaramjad78600@gmail.com>
2024-10-07 23:02:47 +0500

    adding image upload

--- DjangoApp/DjangoApp/urls.py
+++ DjangoApp/DjangoApp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static 
+from django.conf import settings
 from . import views 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,6 @@ urlpatterns = [
     path('about/', views.about),
 path('post/', include('posts.urls'))
 
-]
+] 
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT ) 
+
