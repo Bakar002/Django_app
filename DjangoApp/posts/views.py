953f1b736bfa20a83127f9d383c416470f8aafb1
Bakar002 <abubakaramjad78600@gmail.com>
2024-10-07 14:46:48 +0500

    leason05

--- DjangoApp/posts/views.py
+++ DjangoApp/posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    # Retrieve all posts and order them by 'date' field in descending order.
+    posts = Post.objects.all().order_by('-date')  # '-' indicates descending order
+    return render(request, 'posts/posts_list.html', {'posts': posts})
