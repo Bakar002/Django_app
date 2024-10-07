6b28e3028bd864f86c515c0458df70e10ed25e66
Bakar002 <abubakaramjad78600@gmail.com>
2024-10-08 03:23:33 +0500

    authentication proper

--- DjangoApp/posts/views.py
+++ DjangoApp/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def posts_list(request):
@@ -10,7 +10,11 @@ def posts_list(request):
 
 # Correct indentation for post_page function
 def post_page(request, slug):
-    # Retrieve all posts and order them by 'date' field in descending order.
+    # Retrieve a single post using the slug
     post = Post.objects.get(slug=slug) 
     return render(request, 'posts/posts_page.html', {'post': post})
 
+# Ensure the @login_required decorator and function are correctly indented
+@login_required(login_url='/users/login/')
+def post_new(request):
+    return render(request, 'posts/post_new.html')
