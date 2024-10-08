de42745c75cbeca80fc1792fa45153e1473957d5
Bakar002 <abubakaramjad78600@gmail.com>
2024-10-08 19:08:42 +0500

    complete blog post app

--- DjangoApp/posts/views.py
+++ DjangoApp/posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from . import  forms
 
 # Create your views here.
 def posts_list(request):
@@ -16,5 +17,39 @@ def post_page(request, slug):
 
 # Ensure the @login_required decorator and function are correctly indented
 @login_required(login_url='/users/login/')
+# def post_new(request):
+#     return render(request, 'posts/posts_new.html')
+
+
 def post_new(request):
-    return render(request, 'posts/post_new.html')
+    if request.method == 'POST': 
+        form = forms.CreatePost(request.POST, request.FILES) 
+        if form.is_valid():
+            newpost = form.save(commit=False) 
+            newpost.author = request.user 
+            newpost.save()
+            return redirect('posts:list')
+    else:
+        form = forms.CreatePost()
+    return render(request, 'posts/post_new.html', { 'form': form })
+
+
+
+
+
+
+
+
+
+
+
+    # if request.method == 'POST':
+    #     form = PostForm(request.POST, request.FILES)  # Use request.FILES for image uploads
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('post_list')  # Redirect to the post list or any other view
+    # else:
+    #     form = PostForm()
+    
+    # return render(request, 'add_post.html', {'form': form})
+
