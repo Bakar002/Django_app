c6e566d1843b2255a36f11d24a837a094f594d16
Bakar002 <abubakaramjad78600@gmail.com>
2024-10-08 19:08:42 +0500

    complete blog post app

--- DjangoApp/posts/urls.py
+++ DjangoApp/posts/urls.py
@@ -21,7 +21,6 @@ app_name='posts'
 urlpatterns = [
     path('', views.posts_list,name="list"),
     path('new-post/', views.post_new,name="new-post"),
- 
     path('<slug:slug>', views.post_page,name="page"),
 
 
